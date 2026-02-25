7049aefdb7b1758eaa7e05c5dae3b725966d9bd3
Prakash Jha <prakashjha241@gmail.com>
2026-02-25 13:55:57 +0530

    Add files via upload

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -298,11 +298,11 @@ export default function Navbar() {
           </Button>
           </Link> */}
           {/* End Here */}
-          {/* <Link href="/Result" className="h-10 block md:inline" onClick={() => handleCtaClick("Olympiad-Result-2025", "article_top")}>
+          <Link href="/Result" className="h-10 block md:inline" onClick={() => handleCtaClick("Olympiad-Result-2025", "article_top")}>
             <Button>
               Olympiad Result 2025
             </Button>
-          </Link> */}
+          </Link>
           {pathname === "/" && user && userType ? (
             <Link href={userType === "school-coordinator" ? "/school" : "/team"} className="h-10 block md:inline">
               <Button>
