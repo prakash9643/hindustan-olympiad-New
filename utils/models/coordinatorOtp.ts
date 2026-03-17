1b17f4eca8fd0c2a16c0db41bb0adbd2f57b268b
Prakash Jha <prakashjha241@gmail.com>
2026-03-17 22:54:49 +0530

    Util's Updated

---
+++ utils/models/coordinatorOtp.ts
@@ -0,0 +1,11 @@
+import { Omega } from "lucide-react";
+import mongoose from "mongoose";
+
+const CoordinatorotpSchema = new mongoose.Schema({
+  Coorotp: { type: String, required: true },
+  schoolId: { type: String, required: true },
+  expiresAt: { type: Date, required: true }
+});
+
+export const Coordinatorotp =
+  mongoose.models.Coordinatorotp || mongoose.model("Coordinatorotp", CoordinatorotpSchema);
